8aec24cefbc9c1bae8929952cff88de3ae2feff1
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-20 17:24:56 -0700

    Add styles and colors -- functionality broke again

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,108 +1,92 @@
 import React from "react";
 import styled from "styled-components";
-// import {Button, Input} from "semantic-ui-react";
+import {Header, Icon, Button} from "semantic-ui-react";
 import { withFormik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 import axios from "axios";
 
-// import "semantic-ui-css/semantic.min.css";
+import "semantic-ui-css/semantic.min.css";
 
-const FormOverlay = styled.div`
-   background-color: #efedec80;
-   display: block;
-   z-index: 1;
-   width: 100%;
-   height: 100%;
-   position: fixed;
-   top: 0;
-   left: 0;
-   overflow: auto;
 
-   display: flex;
-   justify-content: center;
-   align-items: center;
-`;
+function LoginForm ({token}) {
+   const FormOverlay = styled.div`
+      /* background-color: #efedec80; */
+      background: #f26656;
+      background: linear-gradient(74deg, #f26656b2 0%, #4b559cb2 100%);
+      z-index: 1;
+      width: 100%;
+      height: 100%;
+      position: fixed;
+      top: 0;
+      left: 0;
+      overflow: auto;
 
-// const MainForm = styled(Form)`
-//    background-color: #4b719c;
-//    border-radius: 20px;
-//    width: 600px;
-//    height: 340px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+   `;
+   const MainForm = styled(Form)`
+      background-color: #efedec;
+      border-radius: 20px;
+      width: 300px;
+      height: 500px;
+      padding: 3rem 1rem;
 
-//    display: flex;
-//    flex-direction: column;
-//    justify-content: center;
-//    align-items: center;
-// `;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+   `;
+   const UserInput = styled(Field)`
+      /* background-color: transparent;
+      border: none;
+      border-bottom: 2px solid #594236; */
+      font-size: 1.5rem;
+      margin: 0.75rem;
+      width: 100%;
+   `;
+   const ButtonContainer = styled.div`
+      width: 100%;
 
-// const UserInput = styled(Input)`
-//    font-size: 1.5rem;
-//    margin: 1rem;
-//    width: 70%;
-// `;
-
-// const ButtonContainer = styled.div`
-//    width: 70%;
-
-//    display: flex;
-//    justify-content: flex-end;
-// `;
-
-function LoginForm () {
+      display: flex;
+      justify-content: flex-end;
+   `;
+   
+   
    return (
       <FormOverlay>
-         <Form>
-            <Field 
+         <MainForm>
+            <Header as="h2" icon textAlign="center">
+               <Icon name="sign-in" circular/>
+               <Header.Content>Login</Header.Content>
+               {/* <Icon name="signup" circular/>
+               <Header.Content>Login</Header.Content> */}
+            </Header>
+            {/* <label>Username</label> */}
+            <UserInput 
                name="username" 
                type="text" 
-               placeholder="Enter a User Name"
+               placeholder="Enter Your User Name"
             />
-            <Field 
-               name="attempt1"
+            <UserInput 
+               name="password1"
                type="password" 
-               placeholder="Type a password"
+               placeholder="Type your password"
             />
-            <Field 
-               name="attempt2"
-               type="password" 
-               placeholder="Type a password"
-            />
-            <div className="button-container">
-               <button type="submit">Register</button>
-            </div>
-         </Form>
+            {
+               (token)
+               ?  <UserInput 
+                     name="password2"
+                     type="password" 
+                     placeholder="Retype your password"
+                  />
+               :  null
+            }
+            <ButtonContainer>
+               <Button size="medium" color="blue" type="submit" >Login</Button>
+            </ButtonContainer>
+         </MainForm>
       </FormOverlay>
-      // <FormOverlay>
-      //    <MainForm>
-      //       <Field 
-      //          name="username" 
-      //          type="text" 
-      //          icon="user" 
-      //          iconPosition="left" 
-      //          placeholder="Enter a User Name" 
-      //          component={UserInput}
-      //       />
-      //       <Field 
-      //          name="attempt1" 
-      //          type="password" 
-      //          icon="key" 
-      //          iconPosition="left" 
-      //          placeholder="Type a password" 
-      //          component={UserInput}
-      //       />
-      //       <Field 
-      //          name="attempt2" 
-      //          type="password" 
-      //          icon="key" 
-      //          iconPosition="left" 
-      //          placeholder="Type a password" 
-      //          component={UserInput}
-      //       />
-      //       <ButtonContainer>
-      //          <Button>Register</Button>
-      //       </ButtonContainer>
-      //    </MainForm>
-      // </FormOverlay>
    );
 }
 
@@ -110,8 +94,8 @@ export default withFormik({
    mapPropsToValues: values => {
       return {
          username: values.username || "",
-         attempt1: values.attempt1 || "",
-         attempt2: values.attempt2 || ""
+         password1: values.attempt1 || "",
+         password2: values.attempt2 || ""
       };
    },
    handleSubmit: values => {
