10376e4776d66e52c1dc0ac091539bf682b783d6
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-20 17:29:13 -0700

    Moved styled-component variables outside of component function -- fixed functionality

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,52 +7,50 @@ import axios from "axios";
 
 import "semantic-ui-css/semantic.min.css";
 
+const FormOverlay = styled.div`
+   /* background-color: #efedec80; */
+   background: #f26656;
+   background: linear-gradient(74deg, #f26656b2 0%, #4b559cb2 100%);
+   z-index: 1;
+   width: 100%;
+   height: 100%;
+   position: fixed;
+   top: 0;
+   left: 0;
+   overflow: auto;
+
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`;
+const MainForm = styled(Form)`
+   background-color: #efedec;
+   border-radius: 20px;
+   width: 300px;
+   height: 500px;
+   padding: 3rem 1rem;
+
+   display: flex;
+   flex-direction: column;
+   justify-content: flex-start;
+   align-items: center;
+`;
+const UserInput = styled(Field)`
+   /* background-color: transparent;
+   border: none;
+   border-bottom: 2px solid #594236; */
+   font-size: 1.5rem;
+   margin: 0.75rem;
+   width: 100%;
+`;
+const ButtonContainer = styled.div`
+   width: 100%;
+
+   display: flex;
+   justify-content: flex-end;
+`;
 
 function LoginForm ({token}) {
-   const FormOverlay = styled.div`
-      /* background-color: #efedec80; */
-      background: #f26656;
-      background: linear-gradient(74deg, #f26656b2 0%, #4b559cb2 100%);
-      z-index: 1;
-      width: 100%;
-      height: 100%;
-      position: fixed;
-      top: 0;
-      left: 0;
-      overflow: auto;
-
-      display: flex;
-      justify-content: center;
-      align-items: center;
-   `;
-   const MainForm = styled(Form)`
-      background-color: #efedec;
-      border-radius: 20px;
-      width: 300px;
-      height: 500px;
-      padding: 3rem 1rem;
-
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: center;
-   `;
-   const UserInput = styled(Field)`
-      /* background-color: transparent;
-      border: none;
-      border-bottom: 2px solid #594236; */
-      font-size: 1.5rem;
-      margin: 0.75rem;
-      width: 100%;
-   `;
-   const ButtonContainer = styled.div`
-      width: 100%;
-
-      display: flex;
-      justify-content: flex-end;
-   `;
-   
-   
    return (
       <FormOverlay>
          <MainForm>
