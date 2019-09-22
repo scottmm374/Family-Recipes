9778074b168bbaf95f000f949bf6fdac15f9cf10
Michelle Scott <scottmm374@gmail.com>
2019-09-22 12:17:52 -0400

    added textarea field and dropdown, verified information is submitting from form

--- src/recipe-component/RecipeForm.js
+++ src/recipe-component/RecipeForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { withFormik, Form, Field } from 'formik';
 import * as yup from 'yup';
-import { Container, Card } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import axios from 'axios';
 
 const RecipeForm = ({ errors, touched, status }) => {
@@ -21,44 +21,42 @@ const RecipeForm = ({ errors, touched, status }) => {
             <Form>
                 <div className="input-field">
                     {touched.title && errors.title && <p>{errors.title}</p>}
-                        <Field type="title" name="title" placeholder="Title" />
+                        <Field type="text" name="title" placeholder="Title" />
                 </div>
                 <div className="input-field">
                     {touched.author && errors.author && <p>{errors.author}</p>}
-                        <Field type="author" name="author" placeholder="Author" />
+                        <Field type="text" name="author" placeholder="Author" />
                 </div>
                 <div className="input-field">
                     {touched.ingredients && errors.ingredients && <p>{errors.ingredients}</p>}
-                        <Field type="ingredients" name="ingredients" placeholder="Ingredients" />
+                        <Field component="textarea" name="ingredients" placeholder="Ingredients" />
                 </div>
                 <div className="input-field">
                     {touched.instructions && errors.instructions && <p>{errors.instructions}</p>}
-                        <Field type="instructions" name="instructions" placeholder="Instructions" />
+                        <Field component="textarea" name="instructions" placeholder="Instructions" />
                 </div>
+
+                // Just intial field for catagory
                 <div className="input-field">
                     {touched.catagories && errors.catagories && <p>{errors.catagories}</p>}
-                        <Field type="catagories" name="catagories" placeholder="Catagory" />
+                        <Field component="select" name="catagories" placeholder="Catagory">
+                            <option value="dinner">Dinner</option>
+                            <option value="lunch">Lunch</option>
+                            <option value="breakfast">Breakfast</option>
+                            <option value="desert">Desert</option>
+                            </Field> 
                 </div>
-
                 <div>
-                    <button>Submit</button>
+                    <button type="submit">Submit</button>
                 </div>
             </Form>
-
-            <Card>
-                <h4>Recipe</h4>
-                {recipes.map(recipe => (
-                    <div key={recipe.author}>
-                        <p> Title: {recipe.title}</p>
-                    </div>
-                ))}
-            </Card>
         </Container>
     );
 }
 
 
 const FormikForm = withFormik({
+   
     mapPropsToValues({ title, author, ingredients, instructions, catagory}) {
         return {
             title: title || "",
@@ -79,11 +77,13 @@ const FormikForm = withFormik({
         catagory: yup.string().required(),
     }),
 
+
     handleSubmit({resetForm, setSubmitting, setStatus}) {
+        
         axios
         .post(" some url")
         .then(res => {
-            // console.log(res, "res");
+            console.log(res, "res");
             setStatus(res.data);
             resetForm();
             setSubmitting(false)
