aac860a36b1370af5815b4c7b7138c7bcf923ad2
AislynnEdmiston <davidandaislynn@gmail.com>
2019-09-24 17:55:19 -0400

    mapped over ingredients to make a list

--- src/components/RecipesCard.js
+++ src/components/RecipesCard.js
@@ -4,6 +4,12 @@ function RecipesCard(props) {
     return (
         <div className='card'>
             <h3>{props.card.title.toUpperCase()}</h3>
+            <p>{props.card.author}</p>
+            {props.card.ingredients.map(ingredient =>
+                <ul>
+                    <li>{ingredient}</li>
+                </ul>    
+            )}
             
         </div>
     )
