c3d52f34dd569a2997101560e5c51f2dedb5e4e3
AislynnEdmiston <davidandaislynn@gmail.com>
2019-09-24 18:39:46 -0400

    added catagory to card

--- src/components/RecipesCard.js
+++ src/components/RecipesCard.js
@@ -4,6 +4,7 @@ function RecipesCard(props) {
     return (
         <div className='card'>
             <h3>{props.card.title.toUpperCase()}</h3>
+            <h4>{props.card.catagories}</h4>
             <p>{props.card.author}</p>
             {props.card.ingredients.map(ingredient =>
                 <ul>
