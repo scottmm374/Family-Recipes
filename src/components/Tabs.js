8d67fa9b6a5a9f9a63d8287e0fb4617eb5fb88ed
AislynnEdmiston <davidandaislynn@gmail.com>
2019-09-25 21:24:39 -0400

    added a tabclass and a tab handler, hardcoded cat.

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import Tab from './Tab';
 
 
 const Tabs = (props) => {
+
+    const tabClass = props.selectedTab === props.tab ? 'tab active-tab' : 'tab';
+
+    const tabHandler = () => {
+        props.changeSelected(props.tab)
+    }
+
+    console.log(props, 'Tabs')    
 
     return(
         <div className="tabs">
             <div className='catagories'>
                 <span className='title'>Catagories</span>
                 
-                {props.tabs.map(tab => (
-                    <div className='tab'><Tab tab={tab}/></div>
-                ))}
+                <div className={tabClass} onClick={tabHandler}>
+                    All
+                </div>
+                <div className={tabClass} onClick={tabHandler}>
+                    Breakfast
+                </div>
+                <div className={tabClass} onClick={tabHandler}>
+                    Lunch
+                </div>
+                <div className={tabClass} onClick={tabHandler}>
+                    Dinner
+                </div>
+                <div className={tabClass} onClick={tabHandler}>
+                    Snacks
+                </div>
+                <div className={tabClass} onClick={tabHandler}>
+                    Desserts
+                </div>
 
             </div>
         </div>
