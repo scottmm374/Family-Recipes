4e6bc52400f05b9cfa24ee2744f17321f0901fb3
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-25 22:30:14 -0700

    Updated RecipiesList and RecipiesCard components to work with App changes

--- src/components/RecipiesList.js
+++ src/components/RecipiesList.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RecipesCard from './RecipesCard';
 import styled from 'styled-components';
 
+const CardsContainer = styled.div`
+   display: flex;
+   flex-flow: row wrap;
+   justify-content: space-between;
+`;
 
-function RecipiesList(props) {
+function RecipiesList({recipes}) {
+   const [displayRecipes, setDisplayRecipes] = useState(recipes || []);
+   console.log(JSON.stringify(recipes, null, 3));
 
-    console.log(props);
-
-    return (
-        <div className='cards-container'>
-            {props.data.map(card => (
-                <RecipesCard card={card} />
+   return (
+      <>
+         <div className="filter-bar">
+            <button>All</button>
+            <button>Breakfast</button>
+            <button>Lunch</button>
+            <button>Dinner</button>
+            <button>Desert</button>
+            <button>Snacks</button>
+         </div>
+         <CardsContainer>
+            {displayRecipes.map(recipe => (
+               // <p key={recipe.id}>{recipe.title}</p>
+               <RecipesCard key={recipe.id} {...recipe} />
             ))}
-        </div>
-    )
+         </CardsContainer>
+      </>
+   )
 }
 
 export default RecipiesList;
