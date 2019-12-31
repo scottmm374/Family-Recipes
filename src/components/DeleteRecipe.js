0008edb21ed720628fbc18115336cb3b35d54b0d
Michelle Scott <scottmm374@gmail.com>
2019-12-31 12:58:56 -0500

    Added background, transparent recipr cards, fonts, minimal button styling

--- src/components/DeleteRecipe.js
+++ src/components/DeleteRecipe.js
@@ -1,30 +1,45 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
+
+import styled from "styled-components";
+
+const DelButton = styled.button`
+  border-radius: 10px;
+  background: #453b3b;
+  color: white;
+  padding: 11px;
+  max-width: 125px;
+  margin-left: 55px;
+
+  margin-top: 1.5rem;
+  &:hover {
+    background: white;
+    color: #453b3b;
+  }
+`;
 
 function DeleteRecipe(props) {
-    console.log("props", props)
+  console.log("props", props);
 
+  const handleDelete = () => {
+    axios
+      .delete(`https://family-cookbook-api.herokuapp.com/recipes/${props.id}`)
+      .then(res => {
+        console.log(
+          "deleting Recipe: ",
+          JSON.stringify(res.data.removed, null, 3)
+        );
 
+        props.deleteRecipe(props.id, props.setDisplayRecipes);
+      })
+      .catch(err => console.log(err));
+  };
 
-    const handleDelete = () => {
-        axios
-            .delete(`https://family-cookbook-api.herokuapp.com/recipes/${props.id}`)
-            .then(res => {
-                console.log('deleting Recipe: ', JSON.stringify(res.data.removed, null, 3))
-  
-
-                props.deleteRecipe(props.id, props.setDisplayRecipes)
-            })
-            .catch(err => console.log(err))
-    }
-           
-
-return (
-    <div> 
-
-        <button onClick={handleDelete}>Delete Recipe</button>
+  return (
+    <div>
+      <DelButton onClick={handleDelete}>Delete Recipe</DelButton>
     </div>
-)
-    }
+  );
+}
 
-export default DeleteRecipe
+export default DeleteRecipe;
--- src/components/RecipeCardModal.js
+++ src/components/RecipeCardModal.js
@@ -1,24 +1,44 @@
-import React from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
-import DinnerImage from '../assets/asparagus-barbecue-bbq-675951.jpg'
+import React from "react";
+import { Button, Header, Image, Modal } from "semantic-ui-react";
+import DinnerImage from "../assets/asparagus-barbecue-bbq-675951.jpg";
 
-const RecipeCardModal = ({ title, author, instructions, ingredients, category,}) => (
-  <Modal trigger={<Button>Show Recipe</Button>}>
+import styled from "styled-components";
+
+const ShowButton = styled.button`
+  border-radius: 10px;
+  background: #453b3b;
+  color: white;
+  padding: 13px;
+  margin-top: 1.5rem;
+  max-width: 126px;
+}
+  &:hover {
+    background: white;
+    color: #453b3b;
+  }
+`;
+
+const RecipeCardModal = ({
+  title,
+  author,
+  instructions,
+  ingredients,
+  category
+}) => (
+  <Modal trigger={<ShowButton>Show Recipe</ShowButton>}>
     <Modal.Header>{title.toUpperCase()}</Modal.Header>
     <Modal.Content image>
-      <Image wrapped size='medium' src={DinnerImage}/>
+      <Image wrapped size="medium" src={DinnerImage} />
       <Modal.Description>
         <Header>Chef: {author}</Header>
         <p>Ingedients: {ingredients}</p>
-        <p>Instructions: 
-         {instructions}
+        <p>
+          Instructions:
+          {instructions}
         </p>
-       
       </Modal.Description>
     </Modal.Content>
   </Modal>
-)
+);
 
-
-
-export default RecipeCardModal
+export default RecipeCardModal;
